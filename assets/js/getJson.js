a5bf2d5b77dae014ef1a5cd3d7e07ffd6f87c2fd
gazandic <gazandic@gmail.com>
2016-02-23 14:00:30 +0700

    crawl from linkedin

---
+++ assets/js/getJson.js
@@ -0,0 +1,42 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "data.json";
+
+xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        myFunction(xmlhttp.responseText);
+    }
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+
+function myFunction(response) {
+    var arr = JSON.parse(response);
+    var i;
+    arr = arr.data;
+    var name = arr[0].fullname;
+    var about = arr[1].about; 
+    var now = arr[2].now; 
+    var place = arr[3].location; 
+    var edu = arr[5].edulist[0].eduplace;
+    edu += arr[5].edulist[0].edufocus;
+    document.title.innerHTML = name; 
+    document.getElementsByClassName("fullname")[0].innerHTML = name; 
+    document.getElementsByClassName("fullname")[1].innerHTML = name;
+    document.getElementsByClassName("fullname")[2].innerHTML = name; 
+    document.getElementById("now").innerHTML = now; 
+    document.getElementById("place").innerHTML = place; 
+    document.getElementById("edu").innerHTML = edu;
+    document.getElementById("about").innerHTML = about;
+    var job = arr[4].joblist;
+    var objob,t;
+    for(i = 0; i < job.length; i++) {
+        objob = document.createElement("div");
+        objob.className="job";
+        t = document.createTextNode(job[i].job);       // Create a text node
+        objob.appendChild(t);                                          // Append the text to <p>
+        document.getElementById("work").appendChild(objob);  
+    }
+    // out += "</table>";
+    // document.getElementById("work").innerHTML = out;
+}
--- linkedincrawler/crawler.js
+++ linkedincrawler/crawler.js
@@ -1,3 +1,4 @@
+
 var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
@@ -11,24 +12,32 @@ request(pageToVisit, function(error, response, body) {
    console.log("Status code: " + response.statusCode);
    if(response.statusCode === 200) {
      // Parse the document body
+     var joblist = [];
+     var result  = [];
+     var hitung =0;
      var $ = cheerio.load(body);
-     console.log("Name:  " + $('#name').text());
+     result.push({fullname: $('#name').text()});
+     result.push({about: $('#summary .description p').text()});
+     result.push({now: $('#profile .headline').text()});
+     result.push({location: $('#profile .locality').text()});
+     //result.push({now: $('#profile .extra-info .org').text()});
      var ite = 0;
      var job, company, duration, location, description ;
    	 while(ite<$('ul.positions').children('li.position').length){
    	 	// console.log($('li.position').children().text);
    	 	job =$('ul.positions').children('li.position').eq(ite).children().first().children('h4.item-title');
-   	 	company = $('ul.positions').children('li.position').eq(ite).children().first().children('h5.item-subtitle');
-     	duration = $('ul.positions').children('li.position').eq(ite).children().eq(1).children('span.date-range');
+      company = $('ul.positions').children('li.position').eq(ite).children().first().children('h5.item-subtitle');
+      duration = $('ul.positions').children('li.position').eq(ite).children().eq(1).children('span.date-range');
         location = $('ul.positions').children('li.position').eq(ite).children().eq(1).children('span.location').contents();
         description = $('ul.positions').children('li.position').eq(ite).children().eq(2).contents();
-     	console.log("job:  " + job.text());
-     	console.log("company:  " + company.text());
-     	console.log("duration:  " + duration.text());
-     	console.log("location:  " + location.text());
-     	console.log("description:  " + description.text());
-     	ite++;
+      joblist.push({job: job.text(), company: company.text(),duration: duration.text(),
+        location: location.text() , description: description.text() });
+      ite++;
+      hitung++;
      }
+      result.push({joblist : joblist});
+
+     var edulist=[];
      ite=0;
      var eduplace, edufocus, duration, description ;
    	 while(ite<$('ul.schools').children('li.school').length){
@@ -37,13 +46,14 @@ request(pageToVisit, function(error, response, body) {
    	 	edufocus = $('ul.schools').children('li.school').eq(ite).children().first().children('h5.item-subtitle');
      	duration = $('ul.schools').children('li.school').eq(ite).children().eq(1).children('span.date-range');
         description = $('ul.schools').children('li.school').eq(ite).children().eq(2).contents();
-     	console.log("eduplace:  " + eduplace.text());
-     	console.log("edufocus:  " + edufocus.text());
-     	console.log("duration:  " + duration.text());
-     	console.log("description:  " + description.text());
+      edulist.push({eduplace: eduplace.text(), edufocus: edufocus.text(),duration: duration.text(),
+        description: description.text() });
      	ite++;
+      hitung++;
      }
+     result.push({edulist : edulist});
      ite = 0;
+     var orglist=[];
      var organization, orgposition, duration, description ;
    	 while(ite<$('section#organizations').children('ul').children('li').length){
    	 	// console.log($('li.position').children().text);
@@ -51,20 +61,32 @@ request(pageToVisit, function(error, response, body) {
    	 	orgposition = $('section#organizations').children('ul').children('li').eq(ite).children().first().children('h5.item-subtitle');
      	duration = $('section#organizations').children('ul').children('li').eq(ite).children().eq(1).children('span.date-range');
         description = $('section#organizations').children('ul').children('li').eq(ite).children().eq(2).contents();
-     	console.log("organizations:  " + organizations.text());
-     	console.log("orgposition:  " + orgposition.text());
-     	console.log("duration:  " + duration.text());
-     	console.log("description:  " + description.text());
+     	orglist.push({organization: organizations.text(), position: orgposition.text(),duration: duration.text(),
+        description: description.text() });
      	ite++;
+      hitung++;
      }
+     result.push({orglist : orglist});
      ite=0;
+     var res;
+     var skills=[];
      var skill ;
    	 while(ite<$('ul.pills').children('li.skill').length){
    	 	// console.log($('li.position').children().text);
    	 	skill =$('ul.pills').children('li.skill').eq(ite).children('a').children('span.wrap');
-   		console.log("skill:  " + skill.text());
+   		skills.push({skill: skill.text()});
      	ite++;
+      hitung++;
      }
-
+     result.push({skills : skills});
+     fs = require('fs');
+     fs.writeFile('data.json', JSON.stringify({data: result},null,hitung), function (err) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+     //console.log(  );
+    //res.contentType('application/json');
+    // console.log(JSON.stringify(result));
    }
 });
